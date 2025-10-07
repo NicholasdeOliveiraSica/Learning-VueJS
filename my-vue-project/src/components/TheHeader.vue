fa8164be0dc779f116f3a4c299cadd3444921d4d
Nicholas Sica <nick.sica16@gmail.com>
2025-10-07 20:48:49 +0000

    First commit

---
+++ my-vue-project/src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <header>
+        Header
+    </header>
+
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style>
+
+    header {
+        color: white;
+        background-image: linear-gradient(left, rgb(69, 108, 179), rgba(69, 107, 179, 0))
+    }
+
+</style>
