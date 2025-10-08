b719178977cfb20123730dd183c556c1fac31cd6
Nicholas Sica <nick.sica16@gmail.com>
2025-10-08 06:17:17 +0000

    Estoudando v-if v-for e v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
+
 <template>
-  <p v-if="show">Texto 1</p>
-  <VueButton tailwind="bg-red-500" @click="show = !show">Clique para alterar o texto</VueButton>
-  <p v-if="!show">Texto 2</p>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <button @click="show = !show">{{ show ? 'Ocultar lista de usuários' : 'Mostrar lista de usuários' }}</button>
+
+  <section v-show="show" class="flex flex-wrap justify-center">
+    <UserCard v-for="user in userArray"
+              :key="user.email"
+              :name="user.name"
+              :age="user.age"
+              :email="user.email"
+              class="font-serif" />
+  </section>
+
 </template>
 
 
 
 <script>
-import VueButton from './components/VueButton.vue';
-import HelloWorld from './components/HelloWorld.vue'
+// import VueButton from './components/VueButton.vue';
+import UserCard from './components/UserCard.vue';
 
 export default {
   data() {
     return {
-      show: false
+      show: true,
+      userArray: [
+        { name: "John Doe", age: 30, email: "john.doe@example.com" },
+        { name: "Jane Smith", age: 25, email: "jane.smith@example.com" },
+        { name: "Alice Johnson", age: 28, email: "alice.johnson@example.com" },
+        { name: "Robert Brown", age: 32, email: "robert.brown@example.com" },
+        { name: "Emily Davis", age: 27, email: "emily.davis@example.com" },
+        { name: "Michael Wilson", age: 35, email: "michael.wilson@example.com" },
+        { name: "Sarah Miller", age: 24, email: "sarah.miller@example.com" },
+        { name: "David Anderson", age: 29, email: "david.anderson@example.com" },
+        { name: "Laura Thompson", age: 31, email: "laura.thompson@example.com" },
+        { name: "Daniel Martinez", age: 26, email: "daniel.martinez@example.com" }
+      ] 
     }
   },
   name: 'App',
-  components: {
-    HelloWorld, 
-    VueButton,
-  }
+  components: { UserCard }
 }
 </script>
 
---
+++ src/components/UserCard.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div :class="containerStyles">
+
+    <img :src="imgURL"
+         :alt="name + ' avatar'"
+          class="w-35 rounded-2xl"
+    />
+
+
+    <div :class="textStyles">
+
+      <h1>{{ name }}</h1>
+      <p>{{ age }} anos</p>
+      <p>{{ email }}</p>
+      <p v-if="isOld">This bro is old</p>
+
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isOld: this.age > 28,
+      containerStyles: "w-110 bg-[#fdfdfd] rounded-2xl shadow-lg m-2 flex gap-5 border border-neutral-200",
+      textStyles: "text-left h-full flex flex-col justify-center p-6 font-bold",
+      imgURL: 'https://testingbot.com/free-online-tools/random-avatar/200?u=' + this.age + this.name + this.email
+    }
+  },
+  name: 'UserCard',
+  props: ['name', 'age', 'email']
+}
+
+</script>
