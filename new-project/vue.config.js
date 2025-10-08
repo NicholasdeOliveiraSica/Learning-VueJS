bb426c2a386b08bcabe8013ed07d891bf5b76d32
Nicholas Sica <nick.sica16@gmail.com>
2025-10-08 00:38:56 +0000

    Update in vue.config

--- new-project/vue.config.js
+++ new-project/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    client: {
+      webSocketURL: {
+        protocol: process.env.HTTPS ? 'wss' : 'ws',
+        hostname: '0.0.0.0',
+        port: process.env.HTTPS ? 443 : 8080,
+      },
+    },
+    allowedHosts: 'all',
+  },
 })
