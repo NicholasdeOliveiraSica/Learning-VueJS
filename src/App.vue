ca4c4658e8536a63f987702e43b82b7c50a170d1
Nicholas Sica <nick.sica16@gmail.com>
2025-10-08 04:51:00 +0000

    atualização de pasta

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
+  <p v-if="show">Texto 1</p>
+  <VueButton tailwind="bg-red-500" @click="show = !show">Clique para alterar o texto</VueButton>
+  <p v-if="!show">Texto 2</p>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+
+
 <script>
+import VueButton from './components/VueButton.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      show: false
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    VueButton,
   }
 }
 </script>
+
+
 
 <style>
 #app {
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+    <header>
+        <div class="header w-10">Header</div>
+    </header>
+</template>
+
+
+
+<script>
+
+    export default {
+        
+    }
+
+</script>
+
+
+
+<style>
+
+    .header {
+        background-image: linear-gradient(to right, rgb(9, 116, 255), rgba(9, 116, 255, 0));
+    }
+
+</style>
