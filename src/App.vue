7e184d1eca22a37db0d455b8b1d33655df0082e6
Nicholas Sica <nick.sica16@gmail.com>
2025-10-08 16:36:55 +0000

    new commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <template>
 
   <div class="bg-neutral-200 flex flex-col items-start p-5 rounded-2xl w-160 m-auto my-10">
-    <input type="text" v-model="name" class="bg-white rounded-lg p-1 mb-4" placeholder="Digite seu nome"/>
+    <input type="text" v-model="name" :class="'rounded-lg p-1 mb-4 ' + (bgGray ? 'bg-blue-200' : 'bg-white')" placeholder="Digite seu nome" @focus="changeBG()" @blur="changeBG()"/>
     <p v-show="name" >Olá {{ name }} seu contador está em {{ count }}</p>
     <div class="flex">
       <button @click="count++" class="font-mono cursor-pointer bg-white rounded-lg p-1 m-1">[ + ]</button>
@@ -12,7 +12,7 @@
 
   <div class="bg-neutral-200 flex flex-col items-start p-5 rounded-2xl w-160 m-auto my-10">
     
-    <select v-model="doces">
+    <select v-model="doces" @mouseover="console.log('Hello World')">
       <option value=""> Escolha uma opção </option>
       <option value="Bala de Goma"> Bala de Goma</option>
       <option value="Bala Fini"> Bala Fini</option>
@@ -28,7 +28,7 @@
   <div class="bg-neutral-200 items-start p-5 rounded-2xl w-160 m-auto my-10">
     
 
-    <label for="cpfNota">Deseja incluir CPF na nota?</label> <br>
+    <label name="cpfNota">Deseja incluir CPF na nota?</label> <br>
     <input type="radio" v-model="cpfNota" value="Sim"/> Sim
     <input type="radio" v-model="cpfNota" value="Não"/> Não <br>
 
@@ -40,13 +40,24 @@
     <div>
       
       <label>Quais suas cores favoritas?</label> <br>
-      <input type="checkbox" v-model="colors" value="Vermelho">Vermelho</input>
-      <input type="checkbox" v-model="colors" value="Azul">Azul</input>
-      <input type="checkbox" v-model="colors" value="Verde">Verde</input>
-      <input type="checkbox" v-model="colors" value="Amarelo">Amarelo</input>
+      <input type="checkbox" v-model="colors" value="Vermelho"/>Vermelho
+      <input type="checkbox" v-model="colors" value="Azul"/>Azul
+      <input type="checkbox" v-model="colors" value="Verde"/>Verde
+      <input type="checkbox" v-model="colors" value="Amarelo"/>Amarelo
 
       <p>Cores favoritas: {{ colors.join(', ') }}</p>
     </div>
+  </div>
+
+  <div class="bg-neutral-200 flex flex-col items-start p-5 rounded-2xl w-160 m-auto my-10 text-start">
+    
+    <div class="flex gap-2">
+      
+      <button @click.once="toggleShow()">Olá</button>
+      <p v-show="onClickShow">Mundo!</p>
+
+    </div>
+
   </div>
 </template>
 
@@ -57,21 +68,12 @@
 //  import UserCard from './components/UserCard.vue';
 
 export default {
+  name: 'App',
   data() {
     return {
+      bgGray: false,
+      onClickShow: false,
       show: true,
-//      userArray: [
-//        { name: "John Doe", age: 30, email: "john.doe@example.com" },
-//        { name: "Jane Smith", age: 25, email: "jane.smith@example.com" },
-//        { name: "Alice Johnson", age: 28, email: "alice.johnson@example.com" },
-//        { name: "Robert Brown", age: 32, email: "robert.brown@example.com" },
-//        { name: "Emily Davis", age: 27, email: "emily.davis@example.com" },
-//        { name: "Michael Wilson", age: 35, email: "michael.wilson@example.com" },
-//       { name: "Sarah Miller", age: 24, email: "sarah.miller@example.com" },
-//        { name: "David Anderson", age: 29, email: "david.anderson@example.com" },
-//        { name: "Laura Thompson", age: 31, email: "laura.thompson@example.com" },
-//        { name: "Daniel Martinez", age: 26, email: "daniel.martinez@example.com" }
-//      ], 
       name: '',
       count: 0,
       doces: "",
@@ -79,9 +81,16 @@ export default {
       colors: []
     }
   },
-  name: 'App',
-  components: {  }
+  methods: {
+    toggleShow() {
+      this.onClickShow = !this.onClickShow;
+    },
+    changeBG() {
+      this.bgGray = !this.bgGray;
+    }
+  },
 }
+
 </script>
 
 
