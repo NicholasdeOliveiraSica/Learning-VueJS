048625f712eee077fe688c24f49df4597826ffc7
Nicholas Sica <nick.sica16@gmail.com>
2025-10-10 14:45:27 +0000

    commit

---
+++ src/containers/CompositionAPI.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="changeNames">Click Me</button>
+        <p>{{ name1 }}</p> <p>{{ name2 }}</p>
+    </div>
+</template>
+
+<script>
+    import { ref } from 'vue';
+
+    export default {
+        name: "CompositionAPI",
+        setup() {
+            const name1 = ref("John");
+            const name2 = "Robert";
+
+            const changeNames = () => {
+                name1.value = "Jane";
+                name2.value = "Alice";
+            };
+
+            return {
+                name1,
+                name2,
+                changeNames
+            }
+        }
+    }
+
+</script>
